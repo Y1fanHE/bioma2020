6fb62d0c199f7e3a3f6074c2245f8c8767c57f95
Y1fanHE <heyf95318@yahoo.co.jp>
2020-01-06 23:36:45 +0900

    Update test

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ import RandomParameterCuckooSearch as RPCS
 
 repeat = 15
 
-problems = ["rastrigin", "different_power", "griewank", "xin_she"]
+problems = ["sphere", "ackley", "rastrigin", "rosenbrock"]
 n_var = 50
 
 n_pop = 20
@@ -33,40 +33,40 @@ for problem_name in problems:
     problem = set_problem(problem_name)
 
     # print table header
-    # print("",
-    #       "{:>5}".format("Seed"),
-    #       "{:>10}".format("BCS"),
-    #       "{:>10}".format("RPCS"),
-    #       "{:>10}".format("PECS"),
-    #       "",
-    #       sep="|")
-    # print("",
-    #       "{:->5}".format(""),
-    #       "{:->10}".format(""),
-    #       "{:->10}".format(""),
-    #       "{:->10}".format(""),
-    #       "",
-    #       sep="|")
+    print("",
+          "{:>5}".format("Seed"),
+          "{:>10}".format("BCS"),
+          "{:>10}".format("RPCS"),
+          "{:>10}".format("PECS"),
+          "",
+          sep="|")
+    print("",
+          "{:->5}".format(""),
+          "{:->10}".format(""),
+          "{:->10}".format(""),
+          "{:->10}".format(""),
+          "",
+          sep="|")
 
-    # # open record file
-    # f = open(f"./tmp/{problem_name}.csv", "a")
+    # open record file
+    f = open(f"./tmp/{problem_name}.csv", "a")
 
-    # # write table header
-    # f.write("Seed,BCS,RPCS,PECS\n")
+    # write table header
+    f.write("Seed,BCS,RPCS,PECS\n")
 
     for seed in range(1000, 1000 + repeat):
 
-        # # Basic Cuckoo Search
-        # X1, F1 =\
-        # BCS.optimize(problem, n_var, n_pop, max_gen, max_eval,
-        #              alpha_1, beta_1, levy_alg, pa,
-        #              seed)
+        # Basic Cuckoo Search
+        X1, F1 =\
+        BCS.optimize(problem, n_var, n_pop, max_gen, max_eval,
+                     alpha_1, beta_1, levy_alg, pa_1,
+                     seed)
 
-        # # Random Parameter Cuckoo Search
-        # X2, F2 =\
-        # RPCS.optimize(problem, n_var, n_pop, max_gen, max_eval,
-        #               alpha_1, betal, betau, levy_alg, pa,
-        #               seed)
+        # Random Parameter Cuckoo Search
+        X2, F2 =\
+        RPCS.optimize(problem, n_var, n_pop, max_gen, max_eval,
+                      alpha_1, betal, betau, levy_alg, pa_1,
+                      seed)
 
         # Parameter Evolution Cuckoo Search
         X3, F3 =\
@@ -76,21 +76,21 @@ for problem_name in problems:
                       alpha_2, beta_2, pa_2,
                       seed)
 
-    #     # print results of three algorithms with the seed
-    #     print("",
-    #         "{:>5}".format(seed),
-    #         "{:>10.3e}".format(min(F1)),
-    #         "{:>10.3e}".format(min(F2)),
-    #         "{:>10.3e}".format(min(F3)),
-    #         "",
-    #         sep="|")
+        # print results of three algorithms with the seed
+        print("",
+            "{:>5}".format(seed),
+            "{:>10.3e}".format(min(F1)),
+            "{:>10.3e}".format(min(F2)),
+            "{:>10.3e}".format(min(F3)),
+            "",
+            sep="|")
         
-    #     # write results of three algorithms with the seed
-    #     s = str(seed) + ","\
-    #         "{:.5e}".format(min(F1)) + "," +\
-    #         "{:.5e}".format(min(F2)) + "," +\
-    #         "{:.5e}".format(min(F3)) + "\n"
-    #     f.write(s)
+        # write results of three algorithms with the seed
+        s = str(seed) + ","\
+            "{:.5e}".format(min(F1)) + "," +\
+            "{:.5e}".format(min(F2)) + "," +\
+            "{:.5e}".format(min(F3)) + "\n"
+        f.write(s)
 
-    # # close record file
-    # f.close()
+    # close record file
+    f.close()
