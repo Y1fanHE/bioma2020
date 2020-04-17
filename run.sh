04ed2d33c5b7b2bc144e513de4e0790f227a78b8
Y1fanHE <heyf95318@yahoo.co.jp>
2020-04-17 23:00:28 +0900

    Add new profile

--- run.sh
+++ run.sh
@@ -1,27 +1,38 @@
-"""
-python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/PECS.yml &
-python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/PECS.yml &
-python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/PECS.yml &
-python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/PECS.yml &
-python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/JACS.yml &
-python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/JACS.yml &
-python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/JACS.yml &
-python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/JACS.yml &
-python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/RPCS.yml &
-python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/RPCS.yml &
-python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/RPCS.yml &
-python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/RPCS.yml &
-"""
+# python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/PECS.yml &
+# python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/PECS.yml &
+# python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/PECS.yml &
+# python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/PECS.yml &
+# python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/JACS.yml &
+# python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/JACS.yml &
+# python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/JACS.yml &
+# python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/JACS.yml &
+# python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/RPCS.yml &
+# python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/RPCS.yml &
+# python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/RPCS.yml &
+# python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/RPCS.yml &
 
-python TestDE.py ./SOPyml/SOP1.yml ./ALGOyml/PEDE.yml &
-python TestDE.py ./SOPyml/SOP2.yml ./ALGOyml/PEDE.yml &
-python TestDE.py ./SOPyml/SOP3.yml ./ALGOyml/PEDE.yml &
-python TestDE.py ./SOPyml/SOP4.yml ./ALGOyml/PEDE.yml &
-python TestDE.py ./SOPyml/SOP1.yml ./ALGOyml/JADE.yml &
-python TestDE.py ./SOPyml/SOP2.yml ./ALGOyml/JADE.yml &
-python TestDE.py ./SOPyml/SOP3.yml ./ALGOyml/JADE.yml &
-python TestDE.py ./SOPyml/SOP4.yml ./ALGOyml/JADE.yml &
-python TestDE.py ./SOPyml/SOP1.yml ./ALGOyml/RPDE.yml &
-python TestDE.py ./SOPyml/SOP2.yml ./ALGOyml/RPDE.yml &
-python TestDE.py ./SOPyml/SOP3.yml ./ALGOyml/RPDE.yml &
-python TestDE.py ./SOPyml/SOP4.yml ./ALGOyml/RPDE.yml &
+# python TestDE.py ./SOPyml/SOP1.yml ./ALGOyml/PEDE.yml &
+# python TestDE.py ./SOPyml/SOP2.yml ./ALGOyml/PEDE.yml &
+# python TestDE.py ./SOPyml/SOP3.yml ./ALGOyml/PEDE.yml &
+# python TestDE.py ./SOPyml/SOP4.yml ./ALGOyml/PEDE.yml &
+# python TestDE.py ./SOPyml/SOP1.yml ./ALGOyml/JADE.yml &
+# python TestDE.py ./SOPyml/SOP2.yml ./ALGOyml/JADE.yml &
+# python TestDE.py ./SOPyml/SOP3.yml ./ALGOyml/JADE.yml &
+# python TestDE.py ./SOPyml/SOP4.yml ./ALGOyml/JADE.yml &
+# python TestDE.py ./SOPyml/SOP1.yml ./ALGOyml/RPDE.yml &
+# python TestDE.py ./SOPyml/SOP2.yml ./ALGOyml/RPDE.yml &
+# python TestDE.py ./SOPyml/SOP3.yml ./ALGOyml/RPDE.yml &
+# python TestDE.py ./SOPyml/SOP4.yml ./ALGOyml/RPDE.yml &
+
+# python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/PECSD.yml &
+# python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/PECSD.yml &
+# python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/PECSD.yml &
+# python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/PECSD.yml &
+# python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/JACSD.yml &
+# python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/JACSD.yml &
+# python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/JACSD.yml &
+# python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/JACSD.yml &
+# python TestCS.py ./SOPyml/SOP1.yml ./ALGOyml/RPCSD.yml &
+# python TestCS.py ./SOPyml/SOP2.yml ./ALGOyml/RPCSD.yml &
+# python TestCS.py ./SOPyml/SOP3.yml ./ALGOyml/RPCSD.yml &
+# python TestCS.py ./SOPyml/SOP4.yml ./ALGOyml/RPCSD.yml &
